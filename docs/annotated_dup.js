ddeec5b922b19f74b5efd631eeec746730f95199
Michael C. Heiber <mike.heiber@gmail.com>
2017-07-31 17:06:45 -0400

    Doxygen Documentation Update
    -added sidebar navigation
    -updated index using the github readme file

---
+++ docs/annotated_dup.js
@@ -0,0 +1,11 @@
+var annotated_dup =
+[
+    [ "Coords", "struct_coords.html", "struct_coords" ],
+    [ "Event", "class_event.html", "class_event" ],
+    [ "Lattice", "class_lattice.html", "class_lattice" ],
+    [ "Object", "class_object.html", "class_object" ],
+    [ "Parameters_Lattice", "struct_parameters___lattice.html", "struct_parameters___lattice" ],
+    [ "Parameters_Simulation", "struct_parameters___simulation.html", "struct_parameters___simulation" ],
+    [ "Simulation", "class_simulation.html", "class_simulation" ],
+    [ "Site", "class_site.html", "class_site" ]
+];
---
+++ docs/navtreedata.js
@@ -0,0 +1,33 @@
+var NAVTREE =
+[
+  [ "KMC_Lattice", "index.html", [
+    [ "KMC_Lattice", "md__r_e_a_d_m_e.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_event_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
